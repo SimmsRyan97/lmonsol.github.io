17d29cfe0ce57580a1ea713131ef39127cdadac4
Ryan <33066159+SimmsRyan97@users.noreply.github.com>
2023-10-01 16:29:29 +0100

    Update stickybits.js

--- scripts/stickybits.js
+++ scripts/stickybits.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         top = _this.offset().top
     // layer z-index properly
     _this.css({
-      'z-index' : 1*i+100,
+      'z-index' : 80*i+100,
       'height' : y + 'px',
       'top' : top + 'px',
       'position' : 'absolute'
